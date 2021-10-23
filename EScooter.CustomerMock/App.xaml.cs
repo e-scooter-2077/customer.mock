dfa01b69790ed09c28af30a4d5b4a61df52d6e03
Samuele <samuele.bura98@gmail.com>
2021-10-23 13:34:51 +0200

    Customer mock app

---
+++ EScooter.CustomerMock/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace EScooter.CustomerMock
+{
+    /// <summary>
+    /// Interaction logic for App.xaml.
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
